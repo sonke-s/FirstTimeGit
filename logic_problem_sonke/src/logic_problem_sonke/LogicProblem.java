9df0ecef0802f1bfad53a6db83f643c4cd378a2a
Jonas Blome <jonas.blome@gmx.de>
2021-04-22 14:28:10 +0200

    logic_problem_sonke/

---
+++ logic_problem_sonke/src/logic_problem_sonke/LogicProblem.java
@@ -0,0 +1,20 @@
+package logic_problem_sonke;
+
+public class LogicProblem {
+	public void main() {
+		
+	}
+	
+	/*
+	 * Given three ints, a b c, one of them is small,
+	 * one is medium and one is large. Return true if the
+	 * three values are evenly spaced, so the difference
+	 * between small and medium is the same as the
+	 * difference between medium and large.
+	 */
+	
+	public boolean evenlySpaced(int a, int b, int c)
+	{
+		return false;
+	}
+}
